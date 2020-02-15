5845034c00afadfabc43cb8d51b026e81e14ef2b
MelanX <meissi97@me.com>
2020-02-15 15:31:58 +0100

    bug fix + new feature

--- src/main/java/de/melanx/toolswap/ToolSwap.java
+++ src/main/java/de/melanx/toolswap/ToolSwap.java
@@ -32,24 +32,33 @@ public class ToolSwap {
         BlockState state = event.getState();
         if (event.getEntity() instanceof PlayerEntity) {
             PlayerEntity player = (PlayerEntity) event.getEntity();
-            for (int i = 0; i < 9; i++) {
-                ItemStack stack = player.inventory.getStackInSlot(i);
-                if (stack.getToolTypes().contains(ToolType.AXE)) {
-                    axe = stack;
-                } else if (stack.getToolTypes().contains(ToolType.PICKAXE)) {
-                    pickaxe = stack;
-                } else if (stack.getToolTypes().contains(ToolType.SHOVEL)) {
-                    shovel = stack;
+            ItemStack heldItem = player.getHeldItemMainhand();
+            if (!player.isSneaking()) {
+                for (int i = 0; i < 9; i++) {
+                    ItemStack stack = player.inventory.getStackInSlot(i);
+                    if (stack.getToolTypes().contains(ToolType.AXE)) {
+                        axe = stack;
+                    } else if (stack.getToolTypes().contains(ToolType.PICKAXE)) {
+                        pickaxe = stack;
+                    } else if (stack.getToolTypes().contains(ToolType.SHOVEL)) {
+                        shovel = stack;
+                    }
                 }
-            }
 
-            Material material = state.getMaterial();
-            if ((material == Material.WOOD || material == Material.BAMBOO || material == Material.GOURD) && axe != null) {
-                player.inventory.currentItem = player.inventory.getSlotFor(axe);
-            } else if ((material == Material.ROCK || material == Material.IRON || material == Material.ANVIL) && pickaxe != null) {
-                player.inventory.currentItem = player.inventory.getSlotFor(pickaxe);
-            } else if ((material == Material.ORGANIC || material == Material.EARTH || material == Material.CLAY || material == Material.SAND || material == Material.SNOW || material == Material.SNOW_BLOCK) && shovel != null) {
-                player.inventory.currentItem = player.inventory.getSlotFor(shovel);
+                Material material = state.getMaterial();
+                if ((material == Material.WOOD || material == Material.BAMBOO || material == Material.GOURD) && axe != null) {
+                    if (!(heldItem.getToolTypes().contains(ToolType.AXE))) {
+                        player.inventory.currentItem = player.inventory.getSlotFor(axe);
+                    }
+                } else if ((material == Material.ROCK || material == Material.IRON || material == Material.ANVIL) && pickaxe != null) {
+                    if (!(heldItem.getToolTypes().contains(ToolType.PICKAXE))) {
+                        player.inventory.currentItem = player.inventory.getSlotFor(pickaxe);
+                    }
+                } else if ((material == Material.ORGANIC || material == Material.EARTH || material == Material.CLAY || material == Material.SAND || material == Material.SNOW || material == Material.SNOW_BLOCK) && shovel != null) {
+                    if (!(heldItem.getToolTypes().contains(ToolType.SHOVEL))) {
+                        player.inventory.currentItem = player.inventory.getSlotFor(shovel);
+                    }
+                }
             }
         }
     }
