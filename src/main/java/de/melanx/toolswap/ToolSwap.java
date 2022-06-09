fac722e5ea6d152af675116fe491e77bb8bf8dca
MelanX <info@melanx.de>
2022-06-09 23:19:46 +0200

    use new display test feature

--- src/main/java/de/melanx/toolswap/ToolSwap.java
+++ src/main/java/de/melanx/toolswap/ToolSwap.java
@@ -1,13 +1,8 @@
 package de.melanx.toolswap;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.network.NetworkConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,10 +16,5 @@ public class ToolSwap {
         if (FMLEnvironment.dist == Dist.CLIENT) {
             new ClientToolSwap();
         }
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-    }
-
-    private void setup(FMLCommonSetupEvent event) {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (remote, isServer) -> true));
     }
 }
