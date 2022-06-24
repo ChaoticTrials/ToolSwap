03f7dc6c0832ff0fcb3c985f28e2feef946ce3b8
MelanX <info@melanx.de>
2022-06-24 16:45:58 +0200

    fix tools not swapping when sneaking when not pressing shift

--- src/main/java/de/melanx/toolswap/ClientToolSwap.java
+++ src/main/java/de/melanx/toolswap/ClientToolSwap.java
@@ -142,7 +142,7 @@ public class ClientToolSwap {
                 List<ItemStack> shears = Lists.newArrayList();
                 BlockState state = event.getState();
                 Block block = state.getBlock();
-                if (ClientConfig.sneakToPrevent.get() || !player.isCrouching()) {
+                if (ClientConfig.sneakToPrevent.get() || !player.isShiftKeyDown()) {
                     if (!state.is(Blocks.COBWEB) &&
                             (ClientConfig.ignoreHarvestLevel.get()
                                     || heldItem.getItem() instanceof DiggerItem
