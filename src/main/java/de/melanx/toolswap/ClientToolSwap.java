de06fed25c371f6a294659562dacece123652cf7
MelanX <info@melanx.de>
2022-07-15 19:30:04 +0200

    fix sneaking doesn't prevent swapping
    closes #36

--- src/main/java/de/melanx/toolswap/ClientToolSwap.java
+++ src/main/java/de/melanx/toolswap/ClientToolSwap.java
@@ -159,7 +159,7 @@ public class ClientToolSwap {
             List<ItemStack> shears = Lists.newArrayList();
             BlockState state = level.getBlockState(pos);
             Block block = state.getBlock();
-            if (ClientConfig.sneakToPrevent.get() || !player.isShiftKeyDown()) {
+            if (!ClientConfig.sneakToPrevent.get() || !player.isShiftKeyDown()) {
                 if (!state.is(Blocks.COBWEB) &&
                         (ClientConfig.ignoreHarvestLevel.get()
                                 && heldItem.getItem() instanceof DiggerItem
