403756a3bfa3c6a4c12e6254c3d3aa23c9653f4d
MelanX <info@melanx.de>
2025-12-09 19:11:37 +0100

    1.21.11

--- src/main/java/de/melanx/toolswap/SwapKeys.java
+++ src/main/java/de/melanx/toolswap/SwapKeys.java
@@ -2,13 +2,13 @@ package de.melanx.toolswap;
 
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.ToggleKeyMapping;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.neoforged.neoforge.common.util.Lazy;
 import org.lwjgl.glfw.GLFW;
 
 public class SwapKeys {
 
-    public static final KeyMapping.Category TOOLSWAP_KEYS = new KeyMapping.Category(ResourceLocation.fromNamespaceAndPath(ToolSwap.MODID, "toolswap_keys"));
+    public static final KeyMapping.Category TOOLSWAP_KEYS = new KeyMapping.Category(Identifier.fromNamespaceAndPath(ToolSwap.MODID, "toolswap_keys"));
 
     public static final Lazy<ToggleKeyMapping> TOGGLE = Lazy.of(() -> new ToggleKeyMapping(
             ToolSwap.MODID + ".key.toggle_toolswap_mode",
